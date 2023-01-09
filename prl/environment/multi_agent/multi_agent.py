11c887910ca390659427e2b078b7ce3240fb8261
sascha <hellovertex@outlook.com>
2023-01-09 22:06:15 +0100

    Trying different vector env approaches

---
+++ prl/environment/multi_agent/multi_agent.py
@@ -0,0 +1,10 @@
+class RayEnv:
+    """https://docs.ray.io/en/latest/rllib/rllib-env.html"""
+    # see util.make_multi_agent_env"""
+
+class TianshouEnv:
+    """https://tianshou.readthedocs.io/en/master/_modules/tianshou/env/venvs.html#SubprocVectorEnv"""
+
+
+class StableBaselinesEnv:
+    """https://stable-baselines3.readthedocs.io/en/master/guide/vec_envs.html"""
